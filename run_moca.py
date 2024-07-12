4a1687b2f6838d696b44428d87616aca61c6f2ee
Olga Balsalobre-Ruza <47603865+olgabalsa@users.noreply.github.com>
2024-07-11 23:01:21 -0700

    Add files via upload

--- run_moca.py
+++ run_moca.py
@@ -1,7 +1,10 @@
 import scripts.inputs as inputs # type: ignore
 import scripts.rv_functions as rv_functions # type: ignore
 import scripts.outputs as outputs # type: ignore
+import scripts.obstime as obstime # type: ignore
+from termcolor import cprint
 import numpy as np
+import pandas as pd
 from astropy.time import Time
 import sys
 
@@ -11,63 +14,50 @@ dic_inputs = inputs.get()
 
 # Initilizing the observing dates
 if dic_inputs['sn'] == 'jds':
-    jd = dic_inputs['jd_sch']
+    jd_sch = pd.read_csv(dic_inputs['path_jd'], sep = ',').jd.values
+    dic_inputs['jd_sch'] = jd_sch
+    jd_i = jd_sch[0]
+    if jd_i < Time(dic_inputs['id'], format = 'isot', scale = 'utc').jd:
+        cprint('ERROR: the first day of your schedule is BEFORE the beginning of the observations. You should increase the -id or change your schedule).', 'red')
+        sys.exit()
 else:
-    jd_i = Time(Time.now(), scale = 'utc').jd
+    jd_i = Time(dic_inputs['id'], format = 'isot', scale = 'utc').jd + 0.5
 
 
-# Architecture of the planetary system
-ecc = dic_inputs['ecc']
-w = dic_inputs['w']
-c = ecc * np.cos(np.radians(w))
-d = ecc * np.sin(np.radians(w))
+# Computing the simulated dataset
+mock_timeseries = {'jd': np.array([]), 'rv': np.array([]), 'erv': np.array([])}
 
 
+# inits of some variables
+measure_of_the_night = 1
+nights_current_block = 1
+number_lost_nights = 0
 
 
+print('Mocking the RVs in process...')
+for ndp in range(dic_inputs['nm']):
 
-
-
-
-
-
-# Observational info
-
-
-# Strategy info
-nrv = dic_inputs['nm']
-cad = dic_inputs['cad']
-
-#########
-# Star info
-Prot = dic_inputs['Prot']
-Pmag = dic_inputs['Pmag']
-
-
-# Let's create those Mock RVs!!!
-mocha_rvs = np.array([])
-mocha_ervs = np.array([])
-mocha_jds = np.array([])
-for ndp in range(nrv):
-
-    rv_i = rv_functions.rv_model(np.array([jd_i]), dic_inputs['num_pl'], dic_inputs['K'], dic_inputs['per'], dic_inputs['t0'], c, d)
+    rv_i = rv_functions.rv_model(np.array([jd_i]), dic_inputs)
     noise_i = np.random.normal(0., dic_inputs['erv']) # jitter cosidered to be of the same magnitude as noise
     rv_i += noise_i
-    erv_i = np.random.normal(erv, 0.2 * dic_inputs['erv'])
+    erv_i = np.random.normal(dic_inputs['erv'], 0.2 * dic_inputs['erv'])
 
-    # store the mock values in arrays
-    mocha_rvs = np.append(mocha_rvs, rv_i)
-    mocha_ervs = np.append(mocha_ervs, erv_i)
-    mocha_jds = np.append(mocha_jds, jd_i)
+    mock_timeseries['jd'] = np.append(mock_timeseries['jd'], jd_i)
+    mock_timeseries['rv'] = np.append(mock_timeseries['rv'], rv_i)
+    mock_timeseries['erv'] = np.append(mock_timeseries['erv'], erv_i)
 
-    jd_i += np.random.normal(cad, 0.15) # new date for the next iteration
+    # Find the next JD to observe
+    jd_i, measure_of_the_night, nights_current_block = obstime.find_new_jd(dic_inputs, jd_i, measure_of_the_night, nights_current_block)
+    print(f'Mock RV #{ndp+1} \u2713')
+
+    if np.isnan(jd_i):
+        break
 
 
 # Outputs
-dir = dic_inputs['dir']
-star = dic_inputs['star']
-outputs.save_ascii(mocha_rvs, mocha_ervs, mocha_jds, dir, star)
-outputs.plot_RV_jd(mocha_jds, num_pl, mocha_rvs, mocha_ervs, K, per, t0, c, d, Prot, Pmag, dir, star)
-
-print('Simulation finished!')
+print('Saving the dataset')
+outputs.save_ascii(mock_timeseries, dic_inputs)
+print('Saving figures')
+outputs.do_plot(mock_timeseries, dic_inputs)
+print('Done!')
 print('')
